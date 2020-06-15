d469ea36e13829c4ea974fcdb4881919d776621c
Amauri Dias <athlon.amauri@gmail.com>
2020-06-15 00:05:51 -0300

    removido dist e docs

--- dist/types/resources/IncidenteRede.d.ts
+++
@@ -1,8 +0,0 @@
-import Resource from './Resource';
-import { IIncidenteRedeConsultaResponse } from './interface/ISimetraResponse';
-import IConfig from './interface/IConfig';
-import { IIncidenteRedeConsultaRequest } from './interface/ISimetraRequest';
-export default class IncidenteRede extends Resource {
-    constructor(config: IConfig);
-    consulta({ POP_IP, ENDER_CDE_NOM, ENDER_BAIR_NOM, ENDER_UF, }: IIncidenteRedeConsultaRequest): Promise<IIncidenteRedeConsultaResponse | any>;
-}

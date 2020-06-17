c5ff86763f9af0c684eeeda55b6a3578fb910265
Andre <andeluisflor@live.com>
2020-06-17 08:38:47 -0300

    fazendo consulta de vencimentos

---
+++ src/resources/Vencimentos.ts
@@ -0,0 +1,23 @@
+import Resource from './Resource';
+import IConfig from './interface/IConfig';
+import { IVencimentosConsultResponse } from './interface/ISimetraResponse';
+import SimetraError from './SimetraError';
+
+export default class Vencimentos extends Resource {
+  constructor(config: IConfig) {
+    super(config);
+  }
+
+  public async consulta(): Promise<IVencimentosConsultResponse | any> {
+    const { data } = await this.callApi({
+      method: 'get',
+      params: { sNomeProc: 'FITTELECOM_PARAMETRO_DIAS_VENCIMENTO_CONSULTAR' },
+    });
+
+    if (!(data.retorno.codigo === '0')) {
+      throw new SimetraError(data.retorno.mensagem);
+    }
+
+    return data;
+  }
+}

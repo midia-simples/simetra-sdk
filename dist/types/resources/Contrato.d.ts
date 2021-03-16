6a16dd60535af29eae28c971cf963dfae015754b
AndrelsFlor <AndrelsFlor@users.noreply.github.com>
2021-03-16 20:38:24 +0000

    Updated docs and build

--- dist/types/resources/Contrato.d.ts
+++ dist/types/resources/Contrato.d.ts
@@ -1,10 +1,11 @@
 import Resource from './Resource';
-import { IContratoCadastrarAnexoResponse, IContratoDesbloquearResponse, IContratoConsultaLinhaMovelResponse } from './interface/ISimetraResponse';
+import { IContratoCadastrarAnexoResponse, IContratoDesbloquearResponse, IContratoConsultaLinhaMovelResponse, ITrocaFormaPagamentoResponse } from './interface/ISimetraResponse';
 import IConfig from './interface/IConfig';
-import { IContratoCadastrarAnexoRequest, IContratoDesbloquearRequest, IContratoConsultaLinhaMovelRequest } from './interface/ISimetraRequest';
+import { IContratoCadastrarAnexoRequest, IContratoDesbloquearRequest, IContratoConsultaLinhaMovelRequest, ITrocaFormaPagamentoRequest } from './interface/ISimetraRequest';
 export default class Contrato extends Resource {
     constructor(config: IConfig);
     cadastrarAnexo({ COD_CNTR, SEQ_CONTRATO_ANEXO_TIPO, ANEXO_BUFFER, ANEXO_NOME, }: IContratoCadastrarAnexoRequest): Promise<IContratoCadastrarAnexoResponse | any>;
     desbloquear({ COD_CNTR, }: IContratoDesbloquearRequest): Promise<IContratoDesbloquearResponse | any>;
     consultarContratoLinhaMovel({ COD_CNTR, DATA_CONSUMO, SEQ_LINHA, }: IContratoConsultaLinhaMovelRequest): Promise<IContratoConsultaLinhaMovelResponse>;
+    trocarFormaPagamento({ COD_CNTR, FORM_PAGTO, BANDEIRACARTAO, CVVCARTAO, DATAVALIDADECARTAO, NOMECARTAO, NUMEROCARTAO, IND_BOLETO_FISICO, }: ITrocaFormaPagamentoRequest): Promise<ITrocaFormaPagamentoResponse>;
 }
